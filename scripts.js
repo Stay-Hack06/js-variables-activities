dff6c8e6f0059a536c72bf960be896d133494e9c
Laura Hackney <laura@anniecannons.com>
2023-07-31 18:22:10 -0400

    initial commit

---
+++ scripts.js
@@ -0,0 +1,47 @@
+/************************ Exercise 1 - Identify Variables ************************/
+
+// Declare Variables Here
+
+
+
+
+
+
+
+
+
+/************************ Exercise 2 - Declaring Variables ************************/
+
+// Declare three variables named firstName, lastName, and age. Assign your own values to each variable. 
+
+
+// Log the variables' values to the console.
+
+
+
+
+
+/************************ Exercise 3 - Swapping Variables  ************************/
+
+// Create two variables, a and b, and assign them any values.
+
+// Swap the values of a and b without using a third variable. 
+
+//Log the values of a and b before and after the swap.
+
+
+
+
+
+/************************ Exercise 4 - Working with Constants ************************/
+
+// Declare a constant variable named PI and set its value to 3.14159. 
+
+// Then, attempt to reassign a new value to PI. 
+
+// Observe the error in the console and write a comment explaining why you can't reassign a constant.
+
+
+
+
+
