160c25c4876a02ea20f29805fb55e020c6279c33
Stay-Hack <staysha.l.hackmann@gmail.com>
2023-08-04 09:45:32 -0700

    complete

--- scripts.js
+++ scripts.js
@@ -3,9 +3,23 @@
 // Declare Variables Here
 
 
-
-
-
+let totalSales;
+let totalExpense;
+let totalProfit;
+let adSpent;
+let upsell;
+let employes;
+let bundels;
+let apps;
+let abandanctCart;
+let status;
+let spent;
+let Return;
+let costConversion
+let costClick
+let CTR;
+let ageGroup
+let location
 
 
 
@@ -13,11 +27,14 @@
 /************************ Exercise 2 - Declaring Variables ************************/
 
 // Declare three variables named firstName, lastName, and age. Assign your own values to each variable. 
+let firstName = 'Staysha';
+let lastName = 'Hackmann';
+let age = '33';
 
 
 // Log the variables' values to the console.
 
-
+console.log(firstName+' '+lastName+' '+age);
 
 
 
@@ -28,6 +45,21 @@
 // Swap the values of a and b. You may use a third variable to hold a value so you don't overwrite it. Use reassignment—don't change the previous lines of code!
 
 // Log the values of a and b before and after the swap.
+
+let a = "apple";
+let b = "banana";
+let c = "candy";
+console.log(a);
+console.log(b);
+console.log(c);
+
+let temp = a;
+a = b;
+b = temp;
+
+console.log(a);
+console.log(b);
+console.log(c);
 
 
 
@@ -40,3 +72,9 @@
 // Then, attempt to reassign a new value to PI. 
 
 // Observe the error in the console and write a comment here explaining why you can't reassign a constant.
+
+const PI = 3.14159;
+PI = 25
+
+// Assignment to constant variable. 
+//Because const assigns a constant variable. It becomes 'permanent' and can't be changed like other variables.
